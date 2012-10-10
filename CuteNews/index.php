48e70e00704b06cb14f64875468f806e0a5d0693
CuteNews <support@cutephp.com>
2012-10-10 10:44:42 +0400

    Autoupdater Robot

--- CuteNews/index.php
+++ CuteNews/index.php
@@ -68,8 +68,10 @@ $password    = $_POST['password'];
 
 // User is banned
 if ( $bandata = user_getban($ip, false))
-     if ($bandata[2] > 3)
+{
+     if ($bandata[1] > 4)
         msg('error', lang('Error!'), getpart('youban', format_date( $bandata[2], 'since-short')));
+}
 
 if ( empty($_SESS['user']))
 {
