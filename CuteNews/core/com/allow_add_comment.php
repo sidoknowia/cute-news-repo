0619a8fc4c581b8c9286278961da33bc360f4cd0
CuteNews <support@cutephp.com>
2013-03-18 02:43:21 -0400

    Autoupdater Robot

--- CuteNews/core/com/allow_add_comment.php
+++ CuteNews/core/com/allow_add_comment.php
@@ -310,10 +310,7 @@
         send_mail($config_notify_email, $subject, $message);
     }
 
-    list($hook_is_used, $URL) = hook('rewrite_add_comment_url', array(false, $URL));
-    if(!$hook_is_used)
-        $URL = $PHP_SELF . build_uri('subaction,id,ucat,archive,start_from:comm_start_from,title', array('showcomments', $id ,$ucat, $archive, $start_from, titleToUrl($news_arr[NEW_TITLE])), false);
-
+    $URL = hook('rewrite_add_comment_url', $PHP_SELF . build_uri('subaction,id,ucat,archive,start_from:comm_start_from,title', array('showcomments', $id ,$ucat, $archive, $start_from, $news_arr[NEW_TITLE]), false));
     echo '<script type="text/javascript">window.location="'.$URL.'";</script>';
 
     // ------------ ALL OK ----------------
